4bfa596d39630f6153c5b21d2f069040a868b136
Josica <josica.ganesamurthy@gmail.com>
2017-09-01 21:08:23 -0400

    Add more sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,11 +14,29 @@ var bio = {
   skills: ['C++', 'Ruby', 'Python', 'Perl', 'Javascript'],
   biopic: 'https://i.imgur.com/GJqlRvw.jpg',
   display: function() {
+    // variable declarations
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    // jquery functions
+    $("#header").prepend(formattedRole);
     $("#header").prepend(formattedName);
+    $("#header").append(formattedBioPic);
+    $("#header").append(formattedWelcomeMessage);
+    $("#topContacts").append(formattedMobile);
+    $("#topContacts").append(formattedEmail);
+    $("#topContacts").append(formattedGithub);
+    $("#topContacts").append(formattedTwitter);
+    $("#topContacts").append(formattedLocation);
   }
 };
 
 // Function calls
 
-bio.display(); 
+bio.display();
